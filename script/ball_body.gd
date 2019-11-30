5b623af9892cac08ad898ef80ccc73128b338785
이용현 <dldydgus1995@naver.com>
2019-11-30 20:07:13 +0900

    버전1 마지막 커밋 조작법을 완전히 바꿀 예정

--- script/ball_body.gd
+++ script/ball_body.gd
@@ -13,16 +13,19 @@ func _process(delta):
 	if global_position.y > get_viewport().size.y or global_position.y < 0:
 		speed.y *= -1
 	global_position += speed"""
+	get_node("../Label").text = ("linear_velocity : "+str(get_linear_velocity ()))
 	ghost_effects()
 	pass
 
 
 func _on_ball_body_body_entered(body):
 	if body.name == "player_body":
-		var cha = get_node("../../player/player_body")
-		apply_impulse(Vector2(0, 0), Vector2(0, -500))#(global_position-cha.global_position)*800)
-		print("ball touch")
-	pass # Replace with function body.
+		#var cha = get_node("../../player/player_body")
+		#apply_impulse(Vector2(0, 0), Vector2(0, -500))#(global_position-cha.global_position)*800)
+		var a = get_linear_velocity()
+		if a.y > 300:
+			set_axis_velocity ( Vector2(0, -300) )
+		pass # Replace with function body.
 
 func ghost_effects():
 	var a = 4
@@ -33,14 +36,3 @@ func ghost_effects():
 	for i in range(1,6):
 		var ghost = "../../ball_ghost/ghost"
 		get_node(ghost+str(i)).global_position = ghost_effect_position[i-1]
-	
-	"""ghost_effect_position[4] = ghost_effect_position[3]
-	$ghost/ghost5.global_position = ghost_effect_position[4]
-	ghost_effect_position[3] = ghost_effect_position[2]
-	$ghost/ghost4.global_position = ghost_effect_position[3]
-	ghost_effect_position[2] = ghost_effect_position[1]
-	$ghost/ghost3.global_position = ghost_effect_position[2]
-	ghost_effect_position[1] = ghost_effect_position[0]
-	$ghost/ghost2.global_position = ghost_effect_position[1]
-	ghost_effect_position[0] = global_position
-	$ghost/ghost1.global_position = ghost_effect_position[0]"""
